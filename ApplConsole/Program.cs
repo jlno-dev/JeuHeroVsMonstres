a87ee04917344aa69395b0bc665b1ad93731e854
jlno-dev <jlno-dev@outlook.fr>
2020-05-24 22:10:54 +0200

    Console presentation for game

---
+++ ApplConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Metier.Classes;
+
+namespace ApplConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Hero hero = new Hero();
+            //hero.AfficherNiveau();
+            //hero.Nom = "HERO";
+            //Console.WriteLine(hero.Nom);
+            //Console.ReadLine();
+        }
+    }
+}
