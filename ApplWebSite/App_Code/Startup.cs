fd33ce7e6081419de6c0193bf3729a144b052a06
jlno-dev <jlno-dev@outlook.fr>
2020-05-24 22:04:27 +0200

    Web site interface

---
+++ ApplWebSite/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ApplWebSite.Startup))]
+namespace ApplWebSite
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
