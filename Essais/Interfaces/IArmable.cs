cd8afac93ce3ece82c89c47f811797dcd7302620
jlno-dev <jlno-dev@outlook.fr>
2020-05-30 11:19:26 +0200

    repertoire contenant la declaration des interfaces

---
+++ Essais/Interfaces/IArmable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Essais.Interfaces
+{
+    public interface IArmable
+    {
+        int Attaquer();
+        int Defendre();
+    }
+}
---
+++ Essais/Interfaces/IEquipable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Essais.Interfaces
+{
+    public interface IEquipable
+    {
+        int Niveau { set; get; }
+        void AugmenterNiveau(int pAugmentation);
+    }
+}
