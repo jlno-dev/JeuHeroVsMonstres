41d6151d7afd2f2879d5d2a90e152f606309b716
jlno <jlno-dev@outlook.fr>
2020-06-07 17:25:15 +0200

    ajout des interfaces iequitable et idisposable

--- Essais/Classes/Element.cs
+++ Essais/Classes/Element.cs
@@ -1,9 +1,11 @@
 ﻿using Essais.TypesDefinis;
+using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Essais.Classes
 {
     
-    public class Element
+    public class Element: IEquatable<Element>, IDisposable
     {
         // ++++ Proprietes
         public TypeElement Type { protected set; get; }
@@ -21,6 +23,47 @@ namespace Essais.Classes
         {
             return "[Element]"; //base.ToString();
         }
+
+        public virtual bool Equals(Element pElem)
+        {
+            return (this.Nom == pElem.Nom && this.Type == pElem.Type);
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects).
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~Element()
+        // {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
         // +++++ Methodes privées
 
         // +++++ Methodes interfaces
